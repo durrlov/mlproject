739e7b065923bc31a814150c5f8140b75bd87623
durrlov <bakhtiar.scr@gmail.com>
2025-05-14 17:36:06 +0600

    Flask App 1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_requirements(file_path:str) -> List[str]:
     this function will return the list of requirements
     '''
     requirements = []
-    with open(file_path) as file_obj:
+    with open(file_path, encoding='utf-8', errors='ignore') as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
 
